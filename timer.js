deeae9c0d5769835fbebf9e161ac1ae3df722740
Bobby Schade <bschade.18@gmail.com>
2020-08-10 11:12:55 -0500

    initial commit

---
+++ timer.js
@@ -0,0 +1,46 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callbacks.onStart) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 20);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.onComplete();
+      this.pause();
+    } else {
+      this.onTick(this.timeRemaining);
+      this.timeRemaining = this.timeRemaining - 0.02;
+    }
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
